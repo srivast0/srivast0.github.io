9be1b5cca4bc431073d80aee30cf68f4eefc6340
Shivam Srivastava <shivam311099@gmail.com>
2022-06-28 11:45:27 +0530

    Uploaded New Files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  // Typing Animation
+ var typed = new Typed(".typing",{
+     strings:["Software Engineer","Java Developer","Back-End Developer","Full Stack Developer"],
+     typeSpeed:100,
+     BackSpeed:60,
+     loop:true
+ })
+ 
